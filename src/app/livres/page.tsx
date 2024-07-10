a1ce098c77142f1cb5f59bca06dc0acd38002f1d
Tea-bow <thibaut.hellinckx@hotmail.com>
2024-07-10 18:20:46 +0200

    removed enum export

--- src/app/livres/page.tsx
+++ src/app/livres/page.tsx
@@ -10,7 +10,7 @@ const lora = Lora({ subsets: ["latin"], weight: "400" });
 const LoraItalic = Lora({ subsets: ["latin"], weight: "400", style: "italic" });
 const Lorabold = Lora({ subsets: ["latin"], weight: "700" });
 
-export enum Category {
+enum Category {
   All = "all",
   Roman = "Roman",
   Thriller = "Thriller",
